b5e3de183e4810f7bba78eb7ddc0d0af803002f6
Hytech842 <rohitkale842@gmail.com>
2026-01-09 14:19:41 +0530

    backend integrated

--- src/components/day_1/RegistrationForm.jsx
+++ src/components/day_1/RegistrationForm.jsx
@@ -1,99 +1,138 @@
-import {useState,React} from 'react'
-import './style/registration.css'
+import { useState, React } from "react";
+import "./style/registration.css";
 import { useNavigate } from "react-router-dom";
 
 function RegistrationForm() {
-    const [formData, setFormData] = useState({
-    Uname: '',
-    age: '',
-    email: '',
-    phoneNo: '',
-    address: '',
-    pincode:'',
-    password:''
-  })
+  const [formData, setFormData] = useState({
+    Uname: "",
+    age: "",
+    email: "",
+    phoneNo: "",
+    address: "",
+    pincode: "",
+    password: "",
+  });
 
   const API_URL = import.meta.env.VITE_API_URL;
+  console.log("API_URL =", API_URL);
+
   const navigate = useNavigate();
 
-  const handleChange=(e)=>{
+  const handleChange = (e) => {
     setFormData({
-        ...formData,
-        [e.target.name]:e.target.value
-    })
-  }
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
 
-  const handleSubtmit = async (e)=>{
+  const handleSubtmit = async (e) => {
     e.preventDefault();
-    try{
-      const response = await fetch(`${API_URL}/api/signup`,{
-        method:"POST",
-        headers:{
-          "Content-Type":"application/json",
+    try {
+      const response = await fetch(`${API_URL}/api/signup`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
         },
         body: JSON.stringify(formData),
       });
-      
+
       const data = await response.json();
-      
-      if(!response.ok){
+
+      if (!response.ok) {
         alert(data.message);
         return;
       }
       alert("registration successfull");
       navigate("/login");
-    } catch(error){
+    } catch (error) {
       alert("srver error. try again");
     }
-
-  }
+  };
 
   return (
     <div className="d-flex justify-content-center align-items-center min-vh-100 bg-light">
       <form className="bg-white p-4 rounded shadow-sm" onSubmit={handleSubtmit}>
-
         <h4 className="text-center mb-4 fw-bold">Registration Form</h4>
 
         <div className="ipgroup mb-3">
-          <input type="text" name='Uname' className="form-control" onChange={handleChange} required />
+          <input
+            type="text"
+            name="Uname"
+            className="form-control"
+            onChange={handleChange}
+            required
+          />
           <label>Enter Name</label>
         </div>
 
         <div className="ipgroup mb-3">
-          <input type="number" name='age' className="form-control" onChange={handleChange} required/>
+          <input
+            type="number"
+            name="age"
+            className="form-control"
+            onChange={handleChange}
+            required
+          />
           <label>Enter Age</label>
         </div>
 
         <div className="ipgroup mb-3">
-          <input type="email" name='email' className="form-control" onChange={handleChange} required/>
+          <input
+            type="email"
+            name="email"
+            className="form-control"
+            onChange={handleChange}
+            required
+          />
           <label>Email Address</label>
         </div>
 
         <div className="ipgroup mb-3">
-          <input type="text" name='phoneNo' className="form-control" onChange={handleChange} required />
+          <input
+            type="text"
+            name="phoneNo"
+            className="form-control"
+            onChange={handleChange}
+            required
+          />
           <label>Phone Number</label>
         </div>
 
         <div className="ipgroup mb-4">
-          <textarea type="text" name='address' className="form-control" onChange={handleChange} required />
+          <textarea
+            type="text"
+            name="address"
+            className="form-control"
+            onChange={handleChange}
+            required
+          />
           <label>Address</label>
         </div>
         <div className="ipgroup mb-3">
-          <input type="text" name='pincode' className="form-control" onChange={handleChange} required />
+          <input
+            type="text"
+            name="pincode"
+            className="form-control"
+            onChange={handleChange}
+            required
+          />
           <label>Pincode</label>
         </div>
         <div className="ipgroup mb-3">
-          <input type="text" name='password' className="form-control" onChange={handleChange} required />
+          <input
+            type="text"
+            name="password"
+            className="form-control"
+            onChange={handleChange}
+            required
+          />
           <label>password</label>
         </div>
 
-        <button className="btn btn-primary w-100 fw-semibold">
-          Submit
-        </button>
-
+        <button className="btn btn-primary w-100 fw-semibold">Submit</button>
       </form>
     </div>
-  )
+  );
 }
 
-export default RegistrationForm
+export default RegistrationForm;
